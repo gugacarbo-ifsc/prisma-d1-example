3e8cc39e41c68618d234232b17f30d54796b8094
alunolages <alunolages@lages.sc>
2025-08-14 09:46:25 -0300

    CC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,10 @@ import { revalidatePath } from "next/cache";
 
 export default async function Home() {
 
-  revalidatePath("/");
-
   const adapter = new PrismaD1((await getCloudflareContext({ async: true })).env.DB)
   const prisma = new PrismaClient({ adapter });
   const users = await prisma.user.findMany();
   const result = JSON.stringify(users, null, 2);
-
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
